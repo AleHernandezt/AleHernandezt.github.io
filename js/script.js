dee40045c7312a8b8bb4fc7e8719280ab7b8c523
Alehernandezt <j.alehernandezt@gmail.com>
2023-05-11 23:50:35 -0400

    ready

--- js/script.js
+++ js/script.js
@@ -41,11 +41,11 @@ const modal_container = document.getElementById('modal-container');
 const close = document.getElementById('close');
 
 
-open.addEventListener('click', () =>{
+open.addEventListener('click', () => {
     modal_container.classList.add('show')
-    
+
 })
 
-close.addEventListener('click', () =>{
+close.addEventListener('click', () => {
     modal_container.classList.remove('show')
 })
