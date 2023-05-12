248f0344a07b42025f82cbed165c9cb8dcc8beed
Alehernandezt <j.alehernandezt@gmail.com>
2023-05-11 23:46:14 -0400

    ready

--- js/script.js
+++ js/script.js
@@ -35,3 +35,17 @@ document.querySelectorAll('header').forEach(links => {
         cursor2.classList.remove('active');
     }
 })
+
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal-container');
+const close = document.getElementById('close');
+
+
+open.addEventListener('click', () =>{
+    modal_container.classList.add('show')
+    
+})
+
+close.addEventListener('click', () =>{
+    modal_container.classList.remove('show')
+})
